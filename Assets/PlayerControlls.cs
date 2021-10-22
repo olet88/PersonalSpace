22cbb79efc0b40992b290af8fac0e067e00ff7c2
Ole Tobiesen <otobiesen@hotmail.com>
2021-10-22 20:26:28 +0100

    ...

--- Assets/PlayerControlls.cs
+++ Assets/PlayerControlls.cs
@@ -7,7 +7,18 @@ public class PlayerControlls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Animator anim = GetComponent<Animator>();
+
+
+
+        // Get a list of the animation clips
+        AnimationClip[] animationClips = anim.runtimeAnimatorController.animationClips;
+
+        // Iterate over the clips and gather their information
+        foreach (AnimationClip animClip in animationClips)
+        {
+            Debug.Log(animClip.name + ": " + animClip.length);
+        }
     }
 
     // Update is called once per frame
