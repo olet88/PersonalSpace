eb603c9fc146efd7cb9e373c075a1ba2a7867dac
Håvard Lapin <havard.lapin@bouvet.no>
2021-10-22 23:00:39 +0200

    initial merge

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -20,36 +20,37 @@ public class Movement : MonoBehaviour
         float speed = 0.5f;
         self.LookAt(player);
 
-        self.position += self.forward * speed * Time.deltaTime;
-        if (Vector3.Distance(self.position, this.transform.position) < 5)
-        {
-            Vector3 direction = self.position - this.transform.position;
-            direction.y = 0;
+        self.rotation *= new Quaternion(0, 90, 0, 0);
+        self.eulerAngles = new Vector3(0, self.eulerAngles.y, 0);
+        // Vector3 direction = self.position - player.position;
+        // direction = new Vector3(0, direction.y, 0);
+        // self.rotation = Quaternion.Slerp(this.transform.rotation,
+        //                             Quaternion.LookRotation(direction), 0.1f);
 
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
-                                        Quaternion.LookRotation(direction), 0.1f);
+        self.position -= self.forward * speed * Time.deltaTime;
+        if (Vector3.Distance(self.position, player.position) < 5)
+        {
+
 
             anim.SetBool("IsIdle", false);
-            if (direction.magnitude > .8)
+            if (Vector3.Distance(self.position, player.position) > 2)
             {
-                speed = 1.0f;
+                speed = 5.0f;
                 anim.SetInteger("legs", 2);
                 anim.SetInteger("arms", 2);
             }
             else
             {
-                speed = 0.5f;
-                anim.SetInteger("legs", 1);
-                anim.SetInteger("arms", 1);
+                speed = 0f;
+                anim.SetInteger("legs", 4);
+                anim.SetInteger("arms", 6);
             }
-
-
         }
         else
         {
-            speed = 0;
-            anim.SetInteger("legs", 4);
-            anim.SetInteger("arms", 6);
+            speed = 2.5f;
+            anim.SetInteger("legs", 1);
+            anim.SetInteger("arms", 1);
         }
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
+        controller.detectCollisions = false;
     }
 
     void Update()
